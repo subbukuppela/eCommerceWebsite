a14e86f26a9f6ca84883b0bad38fe72882134f0f
Subbu Kuppela <subbukuppela@hotmail.com>
2024-01-12 23:58:51 -0700

    First Stage:
    Creating the Project.
    Setting Application DB Context
    Creating Database, Table and Seed Data Migrations
    Add Category Controller, Action Methods,
    Adding Controller in the Layout.html

---
+++ eCommerce/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// Category Model.
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Property : Id.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+        /// <summary>
+        /// Property : Name.
+        /// </summary>
+        [Required]
+        public string? Name { get; set; }
+        /// <summary>
+        /// Property : DisplayOrder.
+        /// </summary>
+        public int DisplayOrder { get; set; }
+    }
+}
